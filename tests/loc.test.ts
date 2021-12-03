c0d873a5fcd05728000a3228f842660d52e42378
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-12-03 15:46:49 +0100

    tests: added test for calling parseJsonHeading()

--- tests/loc.test.ts
+++ tests/loc.test.ts
@@ -1,5 +1,6 @@
 import {readFileSync} from 'fs';
-import type {LcshInterface} from "../resources/LC/lcsh-interface";
+import {parseJsonHeading} from "../src/methods/parseJson";
+import type {headings, LcshInterface} from "../src/interfaces";
 
 function readJson(fileName: string): LcshInterface {
     const currentFile = readFileSync(`tests/testFiles/${fileName}`, {encoding: 'utf-8'});
@@ -13,4 +14,15 @@ test('History URI is correct', () => {
     expect(
         obj['@context'].about
     ).toBe("http://id.loc.gov/authorities/subjects/sh85061227");
+});
+
+test('test 2', () => {
+    const obj = readJson('History.json')
+    const jsonPrefLabel: headings[] = [];
+    const subdivisions: headings[] = [];
+    const jsonUriToPrefLabel = {};
+    parseJsonHeading(obj, jsonPrefLabel, subdivisions, jsonUriToPrefLabel)
+    //expect(
+    //    obj['@context'].about
+    //).toBe("http://id.loc.gov/authorities/subjects/sh85061227");
 });
