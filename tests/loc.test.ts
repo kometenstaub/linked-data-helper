abbc789d9c350ae8fb96fc65b018d8fc29f70377
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-12-04 19:48:12 +0100

    test: add tests

--- tests/loc.test.ts
+++ tests/loc.test.ts
@@ -1,7 +1,6 @@
 import { readFileSync } from 'fs';
 import { parseJsonHeading } from '../src/methods/parseJson';
-import type {headings, LcshInterface, uriToHeading} from '../src/interfaces';
-import {match} from "assert";
+import type { headings, LcshInterface, uriToHeading } from '../src/interfaces';
 
 function readJson(fileName: string): LcshInterface {
     const currentFile = readFileSync(`tests/testFiles/${fileName}`, {
@@ -14,21 +13,66 @@ function jsonParsing(filename: string, snapshot: boolean) {
     const obj = readJson(filename);
     const jsonPrefLabel: headings[] = [];
     const subdivisions: headings[] = [];
-    const jsonUriToPrefLabel: uriToHeading= {};
+    const jsonUriToPrefLabel: uriToHeading = {};
     parseJsonHeading(obj, jsonPrefLabel, subdivisions, jsonUriToPrefLabel);
     if (snapshot) {
-        matchSnapshot(jsonPrefLabel, subdivisions, jsonUriToPrefLabel)
+        matchSnapshot(jsonPrefLabel, subdivisions, jsonUriToPrefLabel);
     } else {
-        return {jsonPrefLabel, subdivisions, jsonUriToPrefLabel }
+        testValues(jsonPrefLabel, subdivisions, jsonUriToPrefLabel, filename);
     }
 }
 
-function matchSnapshot(jsonPrefLabel: headings[], subdivisions: headings[], jsonUriToPrefLabel: uriToHeading) {
+function matchSnapshot(
+    jsonPrefLabel: headings[],
+    subdivisions: headings[],
+    jsonUriToPrefLabel: uriToHeading
+) {
     expect(jsonPrefLabel).toMatchSnapshot();
     expect(subdivisions).toMatchSnapshot();
     expect(jsonUriToPrefLabel).toMatchSnapshot();
 }
 
+function testValues(
+    jsonPrefLabel: headings[],
+    subdivisions: headings[],
+    jsonUriToPrefLabel: uriToHeading,
+    fileName: string
+) {
+    let pL, aL, bt, nt, rt, note, lcc, uri;
+    const heading = jsonPrefLabel[0];
+    const subdivs = subdivisions[0];
+    if (heading !== undefined) {
+        ({ pL, aL, bt, nt, rt, note, lcc, uri } = heading);
+    } else {
+        ({ pL, aL, bt, nt, rt, note, lcc, uri } = subdivs);
+    }
+    if (fileName === 'Archaeology.json') {
+        expect(pL).toBe('Archaeology');
+        expect(aL).toBe('Archeology');
+        expect(lcc).toBe('CC');
+        expect(note).toBe(
+            'Here are entered works on archaeology as a branch of learning. This heading may be divided geographically for works on this branch of learning in a specific place. Works on the antiquities of particular regions, countries, cities, etc. are entered under the name of the place subdivided by [Antiquities.]'
+        );
+        expect(uri).toBe('sh85006507');
+
+        expect(subdivisions).toStrictEqual([]);
+
+        expect(jsonUriToPrefLabel).toStrictEqual({ sh85006507: 'Archaeology' });
+    } else if (fileName === 'History-subdiv.json') {
+        expect(pL).toBe('History');
+        expect(aL).toBe('Frontier troubles');
+        expect(lcc).toStrictEqual(undefined);
+        expect(note).toBe(
+            'Use as a topical subdivision under names of countries, cities, etc., and individual corporate bodies, uniform titles of sacred works, and under classes of persons, ethnic groups, and topical headings.'
+        );
+        expect(uri).toBe('sh99005024');
+
+        expect(jsonUriToPrefLabel).toStrictEqual({ sh99005024: 'History' });
+    } else if (fileName === 'Obsidian.json') {
+        //@ts-expect-error, the object will not be undefined
+        expect(bt[0]).toBe('Volcanic ash, tuff, etc.');
+    }
+}
 
 /**
  * unit test
@@ -49,7 +93,7 @@ test('Snapshot of JSON parsing logic History.json', () => {
 });
 
 test('Snapshot of JSON parsing logic History-subdiv.json', () => {
-    jsonParsing('Archaeology.json', true);
+    jsonParsing('History-subdiv.json', true);
 });
 
 test('Snapshot of JSON parsing logic Archaeology.json', () => {
@@ -64,6 +108,14 @@ test('Snapshot of JSON parsing logic Obsidian.json', () => {
  * more unit tests
  */
 
-test('Archaeology properties of JSON objects', () => {
+test('multiple Archaeology properties of JSON objects', () => {
     jsonParsing('Archaeology.json', false);
+});
+
+test('multiple History-subdiv properties of JSON objects', () => {
+    jsonParsing('History-subdiv.json', false);
+});
+
+test('broader term Obsidian to not be the ID but the real name', () => {
+    jsonParsing('Obsidian.json', false);
 });
