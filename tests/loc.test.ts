2ca7efe79e23c10c2a1c84ee54173b9c16e49683
kometenstaub <83140328+kometenstaub@users.noreply.github.com>
2021-12-04 18:17:30 +0100

    chore: format tests/

--- tests/loc.test.ts
+++ tests/loc.test.ts
@@ -1,27 +1,33 @@
-import {readFileSync} from 'fs';
-import {parseJsonHeading} from "../src/methods/parseJson";
-import type {headings, LcshInterface} from "../src/interfaces";
+import { readFileSync } from 'fs';
+import { parseJsonHeading } from '../src/methods/parseJson';
+import type { headings, LcshInterface } from '../src/interfaces';
 
 function readJson(fileName: string): LcshInterface {
-    const currentFile = readFileSync(`tests/testFiles/${fileName}`, {encoding: 'utf-8'});
+    const currentFile = readFileSync(`tests/testFiles/${fileName}`, {
+        encoding: 'utf-8',
+    });
     return JSON.parse(currentFile);
-
 }
 
-
 test('History URI is correct', () => {
-    const obj = readJson('History.json')
-    expect(
-        obj['@context'].about
-    ).toBe("http://id.loc.gov/authorities/subjects/sh85061227");
+    const obj = readJson('History.json');
+    expect(obj['@context'].about).toBe(
+        'http://id.loc.gov/authorities/subjects/sh85061227'
+    );
 });
 
-test('test 2', () => {
-    const obj = readJson('History.json')
+test('Snapshot of JSON parsing logic', () => {
+    const obj = readJson('History.json');
     const jsonPrefLabel: headings[] = [];
     const subdivisions: headings[] = [];
     const jsonUriToPrefLabel = {};
-    parseJsonHeading(obj, jsonPrefLabel, subdivisions, jsonUriToPrefLabel)
+    const result = parseJsonHeading(
+        obj,
+        jsonPrefLabel,
+        subdivisions,
+        jsonUriToPrefLabel
+    );
+    expect(result).toMatchSnapshot();
     //expect(
     //    obj['@context'].about
     //).toBe("http://id.loc.gov/authorities/subjects/sh85061227");
